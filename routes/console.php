3fcf2529a2e3863b336d7036f30be0b61ff8e730
Mubashar Rashid <mubaxhrdev@gmail.com>
2025-02-22 23:25:45 +0500

    adding daily cron

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Schedule::call((new \App\Services\NewsService())->fetchNewsFromAPI())->hourly();
+Artisan::command('fetch-news', function () {
+    (new \App\Services\NewsService())->fetchNewsFromAPI();
+})->purpose('Fetch News Articles');
